9a0f18a69f9090f541048e46aa5b37bcad0a3424
Pat Haynes <pathaynes32@gmail.com>
2019-09-23 16:09:27 -0700

    test passes

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -8,7 +8,7 @@ jest.mock('../lib/database', () => {
 jest.mock('../lib/document-collection');
 
 const Model = require('../lib/model');
-const Schema = require('../lib/schema');
+const Schema = require('../lib/Schema');
 const { getCollection } = require('../lib/database');
 const DocumentCollection = require('../lib/document-collection');
 
@@ -33,7 +33,6 @@ describe('Model', () => {
   it('creates a schema and document collection', () => {
 
     // assert:
-    
     // Cats.schema should be an instance of Schema, 
     expect(Cats.schema).toBeInstanceOf(Schema);
     // with schemaConfig as saved schema
